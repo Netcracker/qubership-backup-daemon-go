6ba264a058c5a62cf397634a53165a4f4fbe6f4b
Clive Dias <clive.dias@netcracker.com>
2026-03-17 13:22:29 +0530

    fix: update config

--- backup-daemon/cmd/main.go
+++ backup-daemon/cmd/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/Netcracker/qubership-backup-daemon-go/backup-daemon/app/app"
 	"github.com/Netcracker/qubership-backup-daemon-go/backup-daemon/app/config"
+	"github.com/gurkankaymak/hocon"
 	"github.com/jessevdk/go-flags"
 	"go.uber.org/zap"
-	"github.com/gurkankaymak/hocon"
+)
+
+type ConfigType string
+
+const (
+	Full        ConfigType = "FULL"
+	Incremental ConfigType = "INCREMENTAL"
 )
 
 func main() {
@@ -31,63 +39,95 @@ func main() {
 	app.Run()
 }
 
-func loadConfigFile() (config.Config, error) {
-	var cfg config.Config
+func loadConfigFile() (*hocon.Config, error) {
 
 	execPath, err := os.Executable()
 	if err != nil {
-		return cfg, err
+		return nil, err
 	}
 
 	defaultConfig := filepath.Join(filepath.Dir(execPath), "backup-daemon.conf")
 	etcConfig := "/etc/backup-daemon.conf"
 
-	var conf *hocon.Config
-
 	if _, err := os.Stat(etcConfig); err == nil {
 		etcConf, err := hocon.ParseResource(etcConfig)
 		if err != nil {
-			return cfg, err
+			return nil, err
 		}
 
 		defaultConf, err := hocon.ParseResource(defaultConfig)
 		if err != nil {
-			return cfg, err
+			return nil, err
 		}
-
-		conf = etcConf.WithFallback(defaultConf)
-		
-	} else if _, err := os.Stat(defaultConfig); err == nil {
-		conf, err = hocon.ParseResource(defaultConfig)
-		if err != nil {
-			return cfg, err
-		}
-	} else {
-		return cfg, nil
+		return etcConf.WithFallback(defaultConf), nil
 	}
+		
+	if _, err := os.Stat(defaultConfig); err == nil {
+	  return hocon.ParseResource(defaultConfig)
+	}
+	
+	return nil, nil
+}
 
-	cfg.Schedule = conf.GetString("schedule")
-	cfg.EvictionPolicy = conf.GetString("eviction")
-	cfg.GranularEvictionPolicy = conf.GetString("granular_eviction")
-	cfg.StorageRoot = conf.GetString("storage")
-	cfg.BackupCmd = conf.GetString("command")
-	cfg.RestoreCmd = conf.GetString("restore_command")
-	cfg.DbListCmd = conf.GetString("list_instances_in_vault_command")
+func buildConfig(conf *hocon.Config, prefix string) config.Config {
+	var cfg config.Config
+
+	cfg.Schedule = conf.GetString(prefix + "schedule")
+	cfg.EvictionPolicy = conf.GetString(prefix + "eviction")
+	cfg.GranularEvictionPolicy = conf.GetString(prefix + "granular_eviction")
+	cfg.StorageRoot = conf.GetString(prefix + "storage")
+	cfg.BackupCmd = conf.GetString(prefix + "command")
+	cfg.RestoreCmd = conf.GetString(prefix + "restore_command")
+	cfg.DbListCmd = conf.GetString(prefix + "list_instances_in_vault_command")
 	cfg.CustomVars = conf.GetStringSlice("custom_vars")
 
 	cfg.GranularSchedule = conf.GetString("granular_schedule")
 	cfg.ScheduledDBs = conf.GetString("scheduled_dbs")
-	cfg.EvictCmd = conf.GetString("evict_command")
+	cfg.EvictCmd = conf.GetString(prefix + "evict_command")
 	cfg.AllowPrefix = conf.GetBoolean("allow_prefix")
 
-	return cfg, nil
+	if strings.ToLower(conf.GetString("s3_enabled")) == "true" {
+		cfg.S3Enabled = true
+		cfg.S3URL = conf.GetString("s3_url")
+		cfg.S3SslVerify = conf.GetBoolean("s3_ssl_verify")
+	}
+
+	if strings.ToLower(conf.GetString("tls_enabled")) == "true" {
+		cfg.TLSEnabled = conf.GetString("tls_enabled")
+		cfg.CertsPath = conf.GetString("certs_path")
+		cfg.TLSPort = conf.GetInt("tls_port")
+	}
+
+	return cfg
+}
+
+func fetchConfig(conf *hocon.Config, config_type ConfigType) config.Config {
+	prefix := ""
+	if config_type == Incremental {
+		prefix = "incremental_"
+	}
+	return buildConfig(conf, prefix)
 }
 
 func loadConfig() (config config.Config, err error) {
 
-	config, err = loadConfigFile()
+	var conf *hocon.Config
+
+	conf, err = loadConfigFile()
 	if err != nil {
-		_, err = flags.Parse(&config)
+		if _, err = flags.Parse(&config); err !=nil {
+		  return config, err
+		}
+		return config, err
 	}
+
+	if conf != nil {
+		return fetchConfig(conf, Full), nil
+	}
+
+	if _, err = flags.Parse(&config); err !=nil {
+		return config, err
+	}
+	
 	return config, err
 }
