dd7312e4a46b135d629c9260f4c3d2a82f40918f
fibu0125 <filipp.buzid@netcracker.com>
2026-03-18 22:47:06 +0500

    feat: add health check and backup management endpoints, revert api

--- backup-daemon/cmd/main.go
+++ backup-daemon/cmd/main.go
@@ -69,6 +69,10 @@ func loadConfigFile() (*hocon.Config, error) {
 	return nil, nil
 }
 
+func sanitizeString(s string) string {
+	return strings.ReplaceAll(s, "\"", "")
+}
+
 func buildConfig(conf *hocon.Config, prefix string) config.Config {
 	var cfg config.Config
 
@@ -76,29 +80,29 @@ func buildConfig(conf *hocon.Config, prefix string) config.Config {
 	if err != nil {
 
 	}
-	cfg.Schedule = conf.GetString(prefix + "schedule")
-	cfg.EvictionPolicy = conf.GetString(prefix + "eviction")
-	cfg.GranularEvictionPolicy = conf.GetString(prefix + "granular_eviction")
-	cfg.StorageRoot = conf.GetString(prefix + "storage")
-	cfg.BackupCmd = conf.GetString(prefix + "command")
-	cfg.RestoreCmd = conf.GetString(prefix + "restore_command")
-	cfg.DbListCmd = conf.GetString(prefix + "list_instances_in_vault_command")
+	cfg.Schedule = sanitizeString(conf.GetString(prefix + "schedule"))
+	cfg.EvictionPolicy = sanitizeString(conf.GetString(prefix + "eviction"))
+	cfg.GranularEvictionPolicy = sanitizeString(conf.GetString(prefix + "granular_eviction"))
+	cfg.StorageRoot = sanitizeString(conf.GetString(prefix + "storage"))
+	cfg.BackupCmd = sanitizeString(conf.GetString(prefix + "command"))
+	cfg.RestoreCmd = sanitizeString(conf.GetString(prefix + "restore_command"))
+	cfg.DbListCmd = sanitizeString(conf.GetString(prefix + "list_instances_in_vault_command"))
 	cfg.CustomVars = conf.GetStringSlice("custom_vars")
 
-	cfg.GranularSchedule = conf.GetString("granular_schedule")
-	cfg.ScheduledDBs = conf.GetString("scheduled_dbs")
-	cfg.EvictCmd = conf.GetString(prefix + "evict_command")
+	cfg.GranularSchedule = sanitizeString(conf.GetString("granular_schedule"))
+	cfg.ScheduledDBs = sanitizeString(conf.GetString("scheduled_dbs"))
+	cfg.EvictCmd = sanitizeString(conf.GetString(prefix + "evict_command"))
 	cfg.AllowPrefix = conf.GetBoolean("allow_prefix")
 
-	if strings.ToLower(conf.GetString("s3_enabled")) == "true" {
+	if strings.ToLower(sanitizeString(conf.GetString("s3_enabled"))) == "true" {
 		cfg.S3Enabled = true
-		cfg.S3URL = conf.GetString("s3_url")
+		cfg.S3URL = sanitizeString(conf.GetString("s3_url"))
 		cfg.S3SslVerify = conf.GetBoolean("s3_ssl_verify")
 	}
 
-	if strings.ToLower(conf.GetString("tls_enabled")) == "true" {
-		cfg.TLSEnabled = conf.GetString("tls_enabled")
-		cfg.CertsPath = conf.GetString("certs_path")
+	if strings.ToLower(sanitizeString(conf.GetString("tls_enabled"))) == "true" {
+		cfg.TLSEnabled = sanitizeString(conf.GetString("tls_enabled"))
+		cfg.CertsPath = sanitizeString(conf.GetString("certs_path"))
 		cfg.TLSPort = conf.GetInt("tls_port")
 	}
 
